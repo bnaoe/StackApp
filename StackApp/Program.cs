843342274eb368f7b7f15b1582d3f1a1436688ed
bnaoe <bp.naoe@gmail.com>
2021-08-01 17:20:15 -0500

    Add project files.

---
+++ StackApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp
+{
+    public class Stackable : Stack
+    {
+        public Stackable()
+        {
+            Console.WriteLine("Start stack program...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stackable= new Stackable();
+
+            stackable.Push(1);
+            stackable.Push(2);
+            stackable.Push(3);
+            stackable.Push(4);
+            stackable.Push(5);
+
+            foreach (var s in stackable.StackList)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Start pop from Stack..");
+
+            Console.WriteLine(stackable.Pop());
+            Console.WriteLine(stackable.Pop());
+            Console.WriteLine(stackable.Pop());
+            Console.WriteLine(stackable.Pop());
+            Console.WriteLine(stackable.Pop());
+
+            Console.WriteLine("Start Stack Clear...");
+
+            stackable.Clear();
+        }
+    }
+}
---
+++ StackApp/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackApp
+{
+    public class Stack
+    {
+        
+        private List<object> _list = new List<object>();
+        private object PopObject;
+
+        public List<object> StackList{ get => _list;}
+        
+        public void Push(object obj)
+        {
+            if (obj != null) _list.Add(obj);
+            else throw new Exception("Nothing to push here.");
+        }
+
+        public object Pop()
+        {
+            if (_list.Count != 0)
+            {
+                PopObject = _list[_list.Count - 1];
+                _list.Remove(PopObject);
+
+                return PopObject;
+            }
+            else
+            {
+                throw new Exception("No Stack to pop..");
+            }
+        }
+
+        public void Clear()
+        {
+            if (_list.Count != 0) _list.Clear();
+            else throw new Exception("Nothing to clear..");
+        }
+    }
+}
